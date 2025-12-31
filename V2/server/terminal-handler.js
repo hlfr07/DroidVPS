6f9547e3c7a76696451829e321ae5c6658bb4e40
Inmaculada EIRL <hf46078@gmail.com>
2025-12-31 10:09:40 -0500

    QUITANDOI DEPENDENCIA NO APTA PARA PROOT

--- V2/server/terminal-handler.js
+++ V2/server/terminal-handler.js
@@ -1,7 +1,74 @@
-import pty from 'node-pty';
+import { spawn } from 'child_process';
 import os from 'os';
+import { EventEmitter } from 'events';
 
 const terminals = new Map();
+
+class SimpleTerminal extends EventEmitter {
+  constructor(id, shell, cwd) {
+    super();
+    this.id = id;
+    this.shell = shell;
+    this.cwd = cwd;
+    this.process = null;
+    this.buffer = '';
+  }
+
+  spawn() {
+    const shellArgs = this.shell === 'bash' ? ['-i'] : [];
+    
+    this.process = spawn(this.shell, shellArgs, {
+      cwd: this.cwd,
+      env: process.env,
+      shell: false
+    });
+
+    this.process.stdout.on('data', (data) => {
+      const output = data.toString();
+      this.emit('data', output);
+    });
+
+    this.process.stderr.on('data', (data) => {
+      const output = data.toString();
+      this.emit('data', output);
+    });
+
+    this.process.on('exit', (code) => {
+      this.emit('exit', code);
+    });
+
+    this.process.on('error', (error) => {
+      this.emit('data', `\r\nError: ${error.message}\r\n`);
+    });
+
+    return this;
+  }
+
+  write(data) {
+    if (this.process && this.process.stdin.writable) {
+      this.process.stdin.write(data);
+    }
+  }
+
+  kill() {
+    if (this.process) {
+      this.process.kill();
+      this.process = null;
+    }
+  }
+
+  resize(cols, rows) {
+    // No-op for child_process, PTY feature not available in proot
+  }
+
+  onData(callback) {
+    this.on('data', callback);
+  }
+
+  onExit(callback) {
+    this.on('exit', callback);
+  }
+}
 
 export function createTerminal(id, cols = 80, rows = 24) {
   if (terminals.has(id)) {
@@ -9,14 +76,10 @@ export function createTerminal(id, cols = 80, rows = 24) {
   }
 
   const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
+  const cwd = process.env.HOME || process.cwd();
 
-  const term = pty.spawn(shell, [], {
-    name: 'xterm-color',
-    cols: cols,
-    rows: rows,
-    cwd: process.env.HOME || process.cwd(),
-    env: process.env
-  });
+  const term = new SimpleTerminal(id, shell, cwd);
+  term.spawn();
 
   terminals.set(id, term);
 
